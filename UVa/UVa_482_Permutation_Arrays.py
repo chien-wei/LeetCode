25b53693b2a2d36c5230f9e09321aa148cebd76a
chien-wei <chien-wei@outlook.com>
2018-01-16 08:17:18 -0700

    add Uva482

---
+++ UVa/UVa_482_Permutation_Arrays.py
@@ -0,0 +1,22 @@
+
+
+import sys
+
+if __name__ == '__main__':
+    # sys.stdin = open("input.txt")
+    t = int(sys.stdin.readline().strip())
+    for j in range(t):
+        sys.stdin.readline()
+
+        orders = [int(i) for i in sys.stdin.readline().strip().split(' ')]
+        if len(orders) == 0:
+            continue
+        numbers = {}
+        for i, n in enumerate(sys.stdin.readline().strip().split(' ')):
+            numbers[orders[i]] = n
+        ind = sorted(numbers)
+        for i in ind:
+            print(numbers[i])
+        
+        if j != t-1:
+            print()
