818815c16f9dc5ae0be89a45055724239fcb1284
chien-wei <chien-wei@outlook.com>
2018-02-02 16:50:27 -0700

    don't forget the case with more than three pound signs

---
+++ 0331_Verify_Preorder_Serialization_of_a_Binary_Tree.py
@@ -0,0 +1,17 @@
+class Solution:
+    def isValidSerialization(self, preorder):
+        """
+        :type preorder: str
+        :rtype: bool
+        """
+        stack = []
+        for n in preorder.split(','):
+            stack.append(n)
+            while len(stack) > 2 and stack[-1] == '#' and stack[-2] == '#' and stack[-3] != '#':
+                stack.pop()
+                stack.pop()
+                stack.pop()
+                stack.append('#')
+        if len(stack) == 1 and stack[0] == '#':
+            return True
+        return False
