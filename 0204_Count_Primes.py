ced0dc821ff43df8a3044ce1dac13dfc811d0de7
chien-wei <chien-wei@outlook.com>
2018-02-06 15:59:18 -0700

    python way to do list assignment

---
+++ 0204_Count_Primes.py
@@ -0,0 +1,13 @@
+class Solution:
+    def countPrimes(self, n):
+        """
+        :type n: int
+        :rtype: int
+        """
+        if n < 3: return 0
+        isPrime = [True] * n
+        isPrime[0] = isPrime[1] = False
+        for i in range(2, int(n ** 0.5) + 1):
+            if isPrime[i]:
+                isPrime[i*i: n: i] = [False] * len(isPrime[i*i: n: i])
+        return sum(isPrime)
