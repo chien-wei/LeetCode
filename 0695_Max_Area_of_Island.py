0a712c921ac01746f2f3066a952ab422081971fb
chien-wei <chien-wei@outlook.com>
2018-03-02 13:57:19 -0700

    dfs, 2d seen table

---
+++ 0695_Max_Area_of_Island.py
@@ -0,0 +1,25 @@
+class Solution:
+    def maxAreaOfIsland(self, grid):
+        """
+        :type grid: List[List[int]]
+        :rtype: int
+        """
+        seen = [[0 for _ in range(len(grid[0]))] for _ in range(len(grid))]
+        self.count = 0
+        ans = 0
+        def dfs(i, j):
+            if seen[i][j] == 1 or grid[i][j] == 0:
+                return
+            else:
+                self.count += 1
+                seen[i][j] = 1
+                for x, y in [(i-1, j), (i, j-1), (i+1, j), (i, j+1)]:
+                        if x >= 0 and y >= 0 and x < len(grid) and y < len(grid[0]) and grid[x][y] == 1 and seen[x][y] == 0:
+                            dfs(x, y)
+                
+        for i in range(len(grid)):
+            for j in range(len(grid[0])):
+                dfs(i, j)
+                ans = max(ans, self.count)
+                self.count = 0
+        return ans
