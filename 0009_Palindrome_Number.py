2b6f7c52c7346fc6f84ba5e88055d0ff28b10ce6
chien-wei <chien-wei@outlook.com>
2018-03-08 18:08:58 -0700

    simple math

---
+++ 0009_Palindrome_Number.py
@@ -0,0 +1,14 @@
+class Solution:
+    def isPalindrome(self, x):
+        """
+        :type x: int
+        :rtype: bool
+        """
+        if x < 0 or (x != 0 and x % 10 == 0):
+            return False
+        y = 0
+        while x > y:
+            y *= 10
+            y += x % 10
+            x //= 10
+        return (x == y or x == y//10)
