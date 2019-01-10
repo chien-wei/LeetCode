06e951f3b669c402a7ede5d99a1a3d4e22c93d8c
chien-wei <chien-wei@outlook.com>
2019-01-10 15:22:12 -0700

    simple tree

--- 0100_Same_Tree.py
+++ 0100_Same_Tree.py
@@ -15,3 +15,17 @@ class Solution:
         if p and q:
             return p.val == q.val and self.isSameTree(p.left, q.left) and self.isSameTree(p.right, q.right)
         return p is q
+
+# 2019/01/10
+        def check(p, q):
+            if type(p) != type(q) or (p and q and p.val != q.val):
+                return False
+            ans = True
+            if p:
+                ans = ans and check(p.left, q.left) 
+            if q:
+                ans = ans and check(p.right, q.right)
+            return ans
+                
+            
+        return check(p, q)
