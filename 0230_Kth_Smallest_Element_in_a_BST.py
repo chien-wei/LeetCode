11a3106fb14dbe7f0a1ad3bd7316d13d33c49666
chien-wei <chien-wei@outlook.com>
2019-02-06 07:39:30 -0700

    simple bst

---
+++ 0230_Kth_Smallest_Element_in_a_BST.py
@@ -0,0 +1,25 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+class Solution:
+    def kthSmallest(self, root: 'TreeNode', k: 'int') -> 'int':
+        infix = []
+        
+        def traversal(root, infix):
+            if not root:
+                return None
+            l = traversal(root.left, infix) 
+            if l != None:
+                infix.append(l)
+            infix.append(root.val)
+            r = traversal(root.right, infix)
+            if r != None:
+                infix.append(r)
+            #print(infix)
+                
+        traversal(root, infix)
+        return infix[k-1]
