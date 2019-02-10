4e56acf3e673c2c75fa8345f14214298eeee8f0f
chien-wei <chien-wei@outlook.com>
2019-02-10 12:00:14 -0700

    union-find with rank and path compression

--- 0200_Number_of_Islands.py
+++ 0200_Number_of_Islands.py
@@ -1,3 +1,4 @@
+# dfs
 class Solution:
     def numIslands(self, matrix):
         """
@@ -22,3 +23,44 @@ class Solution:
                     dfs(i, j, visited)
                     res += 1
         return res
+
+# union-find
+class Solution:
+    def numIslands(self, matrix):
+        """
+        :type grid: List[List[str]]
+        :rtype: int
+        """
+        if len(matrix) == 0:
+            return 0
+        M, N = len(matrix), len(matrix[0])
+        parent = [i for i in range(M*N)]
+        self.count = 0
+        for i in range(M):
+            for j in range(N):
+                if matrix[i][j] == '1':
+                    self.count += 1
+        
+        def find(id1):
+            if parent[id1] == id1:
+                return id1
+            return find(parent[id1])
+        
+        def union(id1, id2):
+            f1 = find(id1)
+            f2 = find(id2)
+            if f1 != f2:
+                parent[f1] = f2
+                self.count -= 1
+        
+        for i in range(M):
+            for j in range(N):
+                if matrix[i][j] == '1':
+                    for (x, y) in [(i-1, j), (i+1, j), (i, j-1), (i, j+1)]:
+                        if x >= 0 and x < M and y >= 0 and y < N and matrix[x][y] == '1':
+                            id1 = i*N + j
+                            id2 = x*N + y
+                            union(id1, id2)
+        return self.count
+
+# union-find optimized by rank and path compression
