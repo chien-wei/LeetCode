4ca17e227d7a46176f01c6a7e4bb8ba001383e94
chien-wei <chien-wei@outlook.com>
2019-02-16 06:57:05 -0700

    union find

--- 0200_Number_of_Islands.py
+++ 0200_Number_of_Islands.py
@@ -64,3 +64,53 @@ class Solution:
         return self.count
 
 # union-find optimized by rank and path compression
+class Solution:
+    def numIslands(self, matrix):
+        """
+        :type grid: List[List[str]]
+        :rtype: int
+        """
+        if len(matrix) == 0:
+            return 0
+        M, N = len(matrix), len(matrix[0])
+        parent = [i for i in range(M*N)]
+        rank = [0 for i in range(M*N)]
+        
+        self.count = 0
+        for i in range(M):
+            for j in range(N):
+                if matrix[i][j] == '1':
+                    self.count += 1
+        
+        def find(id1):
+            print(id1)
+            if parent[id1] == id1:
+                return id1
+            return find(parent[id1])
+        
+        def union(id1, id2):
+            f1 = find(id1)
+            f2 = find(id2)
+            if rank[f1] > rank[f2]:
+                parent[f2] = f1
+                self.count -= 1
+            elif rank[f2] > rank[f1]:
+                parent[f1] = f2
+                self.count -= 1
+            elif f1 != f2:
+                parent[f2] = f1
+                rank[f1] += 1
+                self.count -= 1
+        
+        for i in range(M):
+            for j in range(N):
+                if matrix[i][j] == '1':
+                    for (x, y) in [(i-1, j), (i+1, j), (i, j-1), (i, j+1)]:
+                        if x >= 0 and x < M and y >= 0 and y < N and matrix[x][y] == '1':
+                            id1 = i*N + j
+                            id2 = x*N + y
+                            union(id1, id2)
+                            #print(parent)
+                            #print(rank)
+        return self.count
+        
