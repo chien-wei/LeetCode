8767274e5f4b680df8b4836c9a7215461576dff8
chien-wei <chien-wei@outlook.com>
2019-03-06 10:23:35 -0700

    simple hash

--- 0003_Longest_Substring_Without_Repeating_Characters.py
+++ 0003_Longest_Substring_Without_Repeating_Characters.py
@@ -13,3 +13,26 @@ class Solution:
             end += 1
             res = max(res, end-start)
         return res
+
+# 2019/03/06 update:
+class Solution:
+    def lengthOfLongestSubstring(self, s):
+        """
+        :type s: str
+        :rtype: int
+        """
+        i, j = 0, 0
+        exist = {}
+        result = 0
+        while j < len(s):
+            if s[j] not in exist:
+                exist[s[j]] = 1
+            else:
+                while i < j and s[i] != s[j]:
+                    exist.pop(s[i])
+                    i += 1
+                i += 1
+            j += 1
+            result = max(result, j-i)
+            
+        return result
