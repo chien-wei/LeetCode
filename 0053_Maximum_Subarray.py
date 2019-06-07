f0a259dfc1904825f28b8a1d72db362a23f18b9e
chien-wei <chien-wei@outlook.com>
2019-06-06 22:44:26 -0700

    accumulated sum

--- 0053_Maximum_Subarray.py
+++ 0053_Maximum_Subarray.py
@@ -1,47 +1,7 @@
 class Solution:
-    def maxSubArray(self, nums):
-        """
-        :type nums: List[int]
-        :rtype: int
-        """
-        if not nums:
-            return 0
-
-        curSum = maxSum = nums[0]
-        for num in nums[1:]:
+    def maxSubArray(self, nums: List[int]) -> int:
+        curSum, maxSum = 0, -float('inf')
+        for num in nums:
             curSum = max(num, curSum + num)
             maxSum = max(maxSum, curSum)
-
         return maxSum
-
-# 2019/01/10:
-# explain this with function mx(i, j)  
-class Solution:
-    def maxSubArray(self, nums):
-        """
-        :type nums: List[int]
-        :rtype: int
-        """
-        
-        start = 0
-        end = 0
-        acc_ind = (0, 0)
-        acc_val = -float('Inf')
-        mx_val = acc_val
-        
-        if len(nums) < 1:
-            return None
-        
-        while end < len(nums):
-            if end == 0:
-                acc_val = max(acc_val, nums[0])
-            else:
-                acc_val += nums[end]
-                if nums[end] > acc_val:
-                    acc_val = nums[end]
-                    start = end
-                acc_ind = (start, end)
-
-            end += 1
-            mx_val = max(mx_val, acc_val)
-        return mx_val
