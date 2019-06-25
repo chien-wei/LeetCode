b0a91ecd3708ba717c3027b26d622fe7f4431f24
chien-wei <chien-wei@outlook.com>
2019-06-24 23:34:10 -0700

    binary tree number of node

--- 0331_Verify_Preorder_Serialization_of_a_Binary_Tree.py
+++ 0331_Verify_Preorder_Serialization_of_a_Binary_Tree.py
@@ -15,3 +15,21 @@ class Solution:
         if len(stack) == 1 and stack[0] == '#':
             return True
         return False
+
+# 2019/06/24 Getting better! O(1) space.
+class Solution:
+    def isValidSerialization(self, preorder: str) -> bool:
+        # root has two child
+        # whenever a child comes, a place was taken, meanwhile, one place more for another child
+        count = 1
+        nodes = preorder.split(",")
+        for i, n in enumerate(nodes):
+            if n != "#":
+                count += 1
+            else:
+                count -= 1
+            if count <= 0 and i != len(nodes) - 1:
+                return False
+        if count != 0:
+            return False
+        return True
